9a832c58958d4ff66c272d9a88e4cb2d203ddf86
bitbandit <oloborama@gmail.com>
2019-02-22 14:20:55 +0300

    Added pages and functionality

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
-var express = require("express");
+var express = require("express"),
+    bodyParser = require('body-parser');
 var router = express.Router();
 
-var path = __dirname +"/views/";
+
+
+var path = __dirname + "/views/";
 
 var app = express();
+app.use(bodyParser.json());
 var list = [];
+var locations = [];
+var map = {};
+var index = 0;
 
-app.listen(3000, ()=>{
-    console.log("Server running on port 3000");
+router.use(function (req, res, next) {
+    console.log("/" + req.method);
+    next();
+});
+router.get("/", function (req, res) {
+    res.sendFile(path + "index.html");
 });
 
-app.get("/",(req,res,next)=>{
-    list.push("Resource Acquired")
+router.get("/map", function (req, res) {
+    res.sendFile(path + "map.html");
+});
+
+app.use("/", router);
+
+app.get("/listLocations", function (req, res, next) {
+    locations = [];
+    for (var i in map) {
+        locations.push(map[i].location);
+    }
+    res.json(locations);
+})
+app.post("/pushLocation", function (req, res, next) {
+    var result = req.body;
+    map[result.user] = result;
+    res.json({
+        status: true,
+        values: map
+    });
+})
+
+app.use("*", function (req, res) {
+    list.push("Yo, what are you doing back here?")
     res.json(list);
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
 });
